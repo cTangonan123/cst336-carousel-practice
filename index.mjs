b6f63fe2b14e55ceb2dedcaceaf1f59f9b1f5745
cTangonan123 <ctangonan@csumb.edu>
2024-12-02 22:10:02 -0800

    feat(modal): added review modal to the searchResults.ejs that has validation, look at TODO in searchResults.js file

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import mysql from 'mysql2/promise';
 
 
 const app = express();
@@ -8,7 +9,19 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded bodies
 app.use(express.json());
+
+const pool = mysql.createPool({                   // Create a pool to connect to the database
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+  connectionLimit: 10,
+  waitForConnections: true
+});
+
+const conn = await pool.getConnection();          // Get a connection from the pool
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -43,6 +56,10 @@ app.post('/watchlist', async (req, res) => {
   const movie_id = req.body.movie_id;
   // check if movie is already in database
   // if not, add it
+  let sql = `SELECT * FROM movie WHERE id = ?`;
+  const [rows] = await conn.query(sql, [movie_id]);
+  
+
   const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=en-US`;
   const options = {
     method: 'GET',
@@ -52,10 +69,28 @@ app.post('/watchlist', async (req, res) => {
     }
   };
 
-  fetch(url, options)
+  let result = await fetch(url, options)
     .then(res => res.json())
-    .then(json => console.log(json)) // where to do stuff
+    .then(data => {
+      console.log(data);
+      return data;
+      
+    }) // where to do stuff
     .catch(err => console.error(err));
+
+    // where we add the movie to the database if it isn't already in there
+    if (rows.length === 0) {
+      sql = `INSERT INTO movie (
+        id, title, release_date, overview, backdrop_path, poster_path, vote_average
+      ) VALUES (
+        ?, ?, ?, ?, ?, ?, ?
+      )`;
+      await conn.query(sql, [result.id, result.title, result.release_date, result.overview, result.backdrop_path, result.poster_path, result.vote_average]);
+    }
+
+    // add the movie to the watchlist
+    let sql2 = `INSERT INTO watchlist (movie_id, user_id) VALUES (?, ?)`;
+    await conn.query(sql2, [movie_id, 1]);
 
   res.json({ message: 'Movie added to your watchlist' });
 });
--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('#addToWatchList').forEach(item => {
-  item.addEventListener('click', event => {
+  item.addEventListener('click', e => {
     //handle click
-    let movie_id = event.currentTarget.dataset.movieId
+    let movie_id = e.currentTarget.dataset.movieId
     console.log(movie_id)
 
     fetch('/watchlist', {
@@ -21,4 +21,61 @@ document.querySelectorAll('#addToWatchList').forEach(item => {
       });
 
   });
+});
+
+// instantiation of bootstrap review modal
+const reviewModal = new bootstrap.Modal(document.querySelector('#reviewModal'));
+
+// specifically for the write review buttons from results
+document.querySelectorAll('#writeReview').forEach(item => {
+  item.addEventListener('click', e => {
+    let movie_id = e.currentTarget.dataset.movieId
+    reviewModal.show()
+    console.log(movie_id)
+    document.querySelector("#rMovieId").value = movie_id
+    document.querySelector("#rUserId").value = 1 // hardcoded for now
+
+
+
+  }); //handle click
+});
+
+// for the save button on the review modal
+document.querySelector("#reviewModal").addEventListener('submit', async(e) => {
+  e.preventDefault()
+  console.log("clicked")
+  // get all values
+  let movie_id = document.querySelector("#rMovieId").value
+  let user_id = document.querySelector("#rUserId").value
+  let title = document.querySelector("#rTitle").value
+  let rating = document.querySelector("#rRating").value
+  let review = document.querySelector("#rReview").value
+  if (rating == "" || review == "" || title == "" || rating > 10 || rating < 0) {
+    alert("Please fill out all fields")
+    return
+  }
+
+  // send fetch request
+  // TODO: add post request /review to index.mjs
+  await fetch('/review', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ movie_id: movie_id, user_id: user_id, title: title, rating: rating, review: review })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      alert(data.message)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  console.log(movie_id, user_id, title, rating, review)
+  // close
+
+
+  reviewModal.hide()
+
 });
